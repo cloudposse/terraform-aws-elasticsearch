d61828437aab195507eb9f3394fe0c9fcdfbb76c
Igor Rodionov <goruha@gmail.com>
2018-10-09 02:55:38 +0600

    Fix ports openes (#11)
    * Fix ports openes
    
    * Pin elasticsearch
    
    * Fix ports openes

--- main.tf
+++ main.tf
@@ -22,7 +22,7 @@ resource "aws_security_group_rule" "ingress_security_groups" {
   description              = "Allow inbound traffic from Security Groups"
   type                     = "ingress"
   from_port                = 0
-  to_port                  = 0
+  to_port                  = 65535
   protocol                 = "tcp"
   source_security_group_id = "${element(var.security_groups, count.index)}"
   security_group_id        = "${join("", aws_security_group.default.*.id)}"
@@ -33,7 +33,7 @@ resource "aws_security_group_rule" "ingress_cidr_blocks" {
   description       = "Allow inbound traffic from CIDR blocks"
   type              = "ingress"
   from_port         = 0
-  to_port           = 0
+  to_port           = 65535
   protocol          = "tcp"
   cidr_blocks       = ["${var.allowed_cidr_blocks}"]
   security_group_id = "${join("", aws_security_group.default.*.id)}"
@@ -44,7 +44,7 @@ resource "aws_security_group_rule" "egress" {
   description       = "Allow all egress traffic"
   type              = "egress"
   from_port         = 0
-  to_port           = 0
+  to_port           = 65535
   protocol          = "tcp"
   cidr_blocks       = ["0.0.0.0/0"]
   security_group_id = "${join("", aws_security_group.default.*.id)}"
