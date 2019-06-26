1631ab3e85446a1c1d55ac489191e5b6426b3273
rverma-nikiai <rohit.verma@niki.ai>
2019-06-26 14:59:11 +0530

    upgraded 0.12

--- main.tf
+++ main.tf
@@ -199,7 +199,7 @@ module "domain_hostname" {
   name      = var.name
   ttl       = 60
   zone_id   = var.dns_zone_id
-  records   = [aws_elasticsearch_domain.default.*.endpoint]
+  records   = aws_elasticsearch_domain.default.*.endpoint
 }
 
 module "kibana_hostname" {
@@ -210,6 +210,6 @@ module "kibana_hostname" {
   name      = var.kibana_subdomain_name
   ttl       = 60
   zone_id   = var.dns_zone_id
-  records   = [aws_elasticsearch_domain.default.*.kibana_endpoint]
+  records   = aws_elasticsearch_domain.default.*.kibana_endpoint
 }
 
