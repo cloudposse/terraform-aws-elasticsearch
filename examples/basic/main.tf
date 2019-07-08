fa26e167e4a7f0aefa83853c1d006eb2fddcaf52
aknysh <andriy.knysh@gmail.com>
2019-07-08 00:33:08 -0400

    Convert to TF 0.12

--- examples/basic/main.tf
+++ examples/basic/main.tf
@@ -1,5 +1,5 @@
 module "elasticsearch" {
-  source                  = "git::https://github.com/cloudposse/terraform-aws-elasticsearch.git?ref=master"
+  source                  = "../../"
   namespace               = "eg"
   stage                   = "dev"
   name                    = "es"
@@ -8,7 +8,7 @@ module "elasticsearch" {
   vpc_id                  = "vpc-XXXXXXXXX"
   subnet_ids              = ["subnet-XXXXXXXXX", "subnet-YYYYYYYY"]
   zone_awareness_enabled  = "true"
-  elasticsearch_version   = "6.2"
+  elasticsearch_version   = "6.5"
   instance_type           = "t2.small.elasticsearch"
   instance_count          = 4
   iam_role_arns           = ["arn:aws:iam::XXXXXXXXX:role/ops", "arn:aws:iam::XXXXXXXXX:role/dev"]
@@ -16,7 +16,7 @@ module "elasticsearch" {
   encrypt_at_rest_enabled = "true"
   kibana_subdomain_name   = "kibana-es"
 
-  advanced_options {
-    "rest.action.multi.allow_explicit_index" = "true"
+  advanced_options = {
+    rest.action.multi.allow_explicit_index = "true"
   }
 }
--- variables.tf
+++ variables.tf
@@ -1,78 +1,80 @@
 variable "namespace" {
-  type        = "string"
+  type        = string
   description = "Namespace (e.g. `eg` or `cp`)"
+  default     = ""
 }
 
 variable "stage" {
-  type        = "string"
+  type        = string
   description = "Stage (e.g. `prod`, `dev`, `staging`)"
+  default     = ""
 }
 
 variable "name" {
-  type        = "string"
+  type        = string
   description = "Name of the application"
 }
 
 variable "delimiter" {
-  type        = "string"
+  type        = string
   default     = "-"
   description = "Delimiter to be used between `namespace`, `stage`, `name` and `attributes`"
 }
 
 variable "attributes" {
-  type        = "list"
+  type        = list(string)
   default     = []
   description = "Additional attributes (e.g. `1`)"
 }
 
 variable "tags" {
-  type        = "map"
+  type        = map(string)
   default     = {}
   description = "Additional tags (e.g. map(`BusinessUnit`,`XYZ`)"
 }
 
 variable "enabled" {
-  type        = "string"
-  default     = "true"
+  type        = bool
+  default     = true
   description = "Set to false to prevent the module from creating any resources"
 }
 
 variable "security_groups" {
-  type        = "list"
+  type        = list(string)
   default     = []
   description = "List of security group IDs to be allowed to connect to the cluster"
 }
 
 variable "allowed_cidr_blocks" {
-  type        = "list"
+  type        = list(string)
   default     = []
   description = "List of CIDR blocks to be allowed to connect to the cluster"
 }
 
 variable "vpc_id" {
-  type        = "string"
+  type        = string
   description = "VPC ID"
 }
 
 variable "subnet_ids" {
-  type        = "list"
-  description = "Subnet ids"
+  type        = list(string)
+  description = "Subnet IDs"
 }
 
 variable "dns_zone_id" {
-  type        = "string"
+  type        = string
   default     = ""
   description = "Route53 DNS Zone ID to add hostname records for Elasticsearch domain and Kibana"
 }
 
 variable "elasticsearch_version" {
-  type        = "string"
+  type        = string
   default     = "6.5"
   description = "Version of Elasticsearch to deploy"
 }
 
 variable "instance_type" {
-  type        = "string"
+  type        = string
   default     = "t2.small.elasticsearch"
   description = "Elasticsearch instance type for data nodes in the cluster"
 }
@@ -83,135 +85,139 @@ variable "instance_count" {
 }
 
 variable "iam_role_arns" {
-  type        = "list"
+  type        = list(string)
   default     = []
   description = "List of IAM role ARNs to permit access to the Elasticsearch domain"
 }
 
 variable "iam_authorizing_role_arns" {
-  type        = "list"
+  type        = list(string)
   default     = []
   description = "List of IAM role ARNs to permit to assume the Elasticsearch user role"
 }
 
 variable "iam_actions" {
-  type        = "list"
+  type        = list(string)
   default     = []
   description = "List of actions to allow for the IAM roles, _e.g._ `es:ESHttpGet`, `es:ESHttpPut`, `es:ESHttpPost`"
 }
 
 variable "zone_awareness_enabled" {
-  type        = "string"
-  default     = "true"
+  type        = bool
+  default     = true
   description = "Enable zone awareness for Elasticsearch cluster"
 }
 
 variable "ebs_volume_size" {
+  type        = number
   description = "Optionally use EBS volumes for data storage by specifying volume size in GB"
   default     = 0
 }
 
 variable "ebs_volume_type" {
-  type        = "string"
+  type        = string
   default     = "gp2"
   description = "Storage type of EBS volumes"
 }
 
 variable "ebs_iops" {
+  type        = number
   default     = 0
   description = "The baseline input/output (I/O) performance of EBS volumes attached to data nodes. Applicable only for the Provisioned IOPS EBS volume type"
 }
 
 variable "encrypt_at_rest_enabled" {
-  type        = "string"
-  default     = "true"
+  type        = bool
+  default     = true
   description = "Whether to enable encryption at rest"
 }
 
 variable "encrypt_at_rest_kms_key_id" {
-  type        = "string"
+  type        = string
   default     = ""
-  description = "The KMS key id to encrypt the Elasticsearch domain with. If not specified, then it defaults to using the AWS/Elasticsearch service KMS key"
+  description = "The KMS key ID to encrypt the Elasticsearch domain with. If not specified, then it defaults to using the AWS/Elasticsearch service KMS key"
 }
 
 variable "log_publishing_index_enabled" {
-  type        = "string"
-  default     = "false"
+  type        = bool
+  default     = false
   description = "Specifies whether log publishing option for INDEX_SLOW_LOGS is enabled or not"
 }
 
 variable "log_publishing_search_enabled" {
-  type        = "string"
-  default     = "false"
+  type        = bool
+  default     = false
   description = "Specifies whether log publishing option for SEARCH_SLOW_LOGS is enabled or not"
 }
 
 variable "log_publishing_application_enabled" {
-  type        = "string"
-  default     = "false"
+  type        = bool
+  default     = false
   description = "Specifies whether log publishing option for ES_APPLICATION_LOGS is enabled or not"
 }
 
 variable "log_publishing_index_cloudwatch_log_group_arn" {
-  type        = "string"
+  type        = string
   default     = ""
   description = "ARN of the CloudWatch log group to which log for INDEX_SLOW_LOGS needs to be published"
 }
 
 variable "log_publishing_search_cloudwatch_log_group_arn" {
-  type        = "string"
+  type        = string
   default     = ""
-  description = "ARN of the CloudWatch log group to which log for SEARCH_SLOW_LOGS  needs to be published"
+  description = "ARN of the CloudWatch log group to which log for SEARCH_SLOW_LOGS needs to be published"
 }
 
 variable "log_publishing_application_cloudwatch_log_group_arn" {
-  type        = "string"
+  type        = string
   default     = ""
   description = "ARN of the CloudWatch log group to which log for ES_APPLICATION_LOGS needs to be published"
 }
 
 variable "automated_snapshot_start_hour" {
+  type        = number
   description = "Hour at which automated snapshots are taken, in UTC"
   default     = 0
 }
 
 variable "dedicated_master_enabled" {
-  type        = "string"
-  default     = "false"
+  type        = bool
+  default     = false
   description = "Indicates whether dedicated master nodes are enabled for the cluster"
 }
 
 variable "dedicated_master_count" {
+  type        = number
   description = "Number of dedicated master nodes in the cluster"
   default     = 0
 }
 
 variable "dedicated_master_type" {
-  type        = "string"
+  type        = string
   default     = "t2.small.elasticsearch"
   description = "Instance type of the dedicated master nodes in the cluster"
 }
 
 variable "advanced_options" {
-  type        = "map"
+  type        = map(string)
   default     = {}
   description = "Key-value string pairs to specify advanced configuration options"
 }
 
 variable "kibana_subdomain_name" {
-  type        = "string"
+  type        = string
   default     = "kibana"
   description = "The name of the subdomain for Kibana in the DNS zone (_e.g._ `kibana`, `ui`, `ui-es`, `search-ui`, `kibana.elasticsearch`)"
 }
 
 variable "create_iam_service_linked_role" {
-  type        = "string"
-  default     = "true"
+  type        = bool
+  default     = true
   description = "Whether to create `AWSServiceRoleForAmazonElasticsearchService` service-linked role. Set it to `false` if you already have an ElasticSearch cluster created in the AWS account and AWSServiceRoleForAmazonElasticsearchService already exists. See https://github.com/terraform-providers/terraform-provider-aws/issues/5218 for more info"
 }
 
 variable "node_to_node_encryption_enabled" {
-  type        = "string"
-  default     = "false"
+  type        = bool
+  default     = false
   description = "Whether to enable node-to-node encryption"
 }
