98b669588b79a60945df9e385322172f5de193dd
Josh Larsen <josh@betterworks.com>
2020-02-11 12:25:05 -0700

    fixing terraform issue with zone count

--- main.tf
+++ main.tf
@@ -125,6 +125,10 @@ resource "aws_elasticsearch_domain" "default" {
     dedicated_master_count   = var.dedicated_master_count
     dedicated_master_type    = var.dedicated_master_type
     zone_awareness_enabled   = var.zone_awareness_enabled
+
+    zone_awareness_config {
+      availability_zone_count = length(var.subnet_ids)
+    }
   }
 
   node_to_node_encryption {
