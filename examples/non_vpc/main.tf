61cca5310d21a40214d98b7cfc5b5c0533dbb6e0
Marcin Brański <marcin@cloudposse.com>
2020-06-15 16:17:16 +0200

    Introduce vpc_enabled variable to mitigate problems with ES spinning in VPC

---
+++ examples/non_vpc/main.tf
@@ -0,0 +1,22 @@
+module "elasticsearch" {
+  source                  = "../../"
+  namespace               = "eg"
+  stage                   = "dev"
+  name                    = "es"
+  dns_zone_id             = "Z14EN2YD427LRQ"
+  security_groups         = ["sg-XXXXXXXXX", "sg-YYYYYYYY"]
+  vpc_id                  = "vpc-XXXXXXXXX"
+  subnet_ids              = ["subnet-XXXXXXXXX", "subnet-YYYYYYYY"]
+  zone_awareness_enabled  = "true"
+  elasticsearch_version   = "6.5"
+  instance_type           = "t2.small.elasticsearch"
+  instance_count          = 4
+  iam_role_arns           = ["arn:aws:iam::XXXXXXXXX:role/ops", "arn:aws:iam::XXXXXXXXX:role/dev"]
+  iam_actions             = ["es:ESHttpGet", "es:ESHttpPut", "es:ESHttpPost"]
+  encrypt_at_rest_enabled = "true"
+  kibana_subdomain_name   = "kibana-es"
+
+  advanced_options = {
+    rest.action.multi.allow_explicit_index = "true"
+  }
+}
