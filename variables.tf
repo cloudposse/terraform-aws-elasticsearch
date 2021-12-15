c04e16c075129b4170684c194d65f57dcff894b9
Morre Meyer <morre@mor.re>
2021-12-15 18:26:53 +0100

    feat: add validation to availability_zone_count (#122)
    This ensures that availability_zone_count is only set to allowed values, see https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/elasticsearch_domain#availability_zone_count.

--- variables.tf
+++ variables.tf
@@ -116,6 +116,11 @@ variable "availability_zone_count" {
   type        = number
   default     = 2
   description = "Number of Availability Zones for the domain to use."
+
+  validation {
+    condition     = contains([2, 3], var.availability_zone_count)
+    error_message = "The availibility zone count must be 2 or 3."
+  }
 }
 
 variable "ebs_volume_size" {
