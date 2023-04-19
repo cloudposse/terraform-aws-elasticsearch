0d93d905938fa630f04f5f000724971d6c6521d3
Jacob Hudson <jacob.hudson@vortexa.com>
2023-04-19 16:13:38 -0500

    set validation on the condition of Zone Awareness

--- variables.tf
+++ variables.tf
@@ -122,6 +122,11 @@ variable "availability_zone_count" {
   type        = number
   default     = 2
   description = "Number of Availability Zones for the domain to use."
+
+  validation {
+    condition     = contains([2, 3], var.availability_zone_count) && var.zone_awareness_enabled == "true"
+    error_message = "The availibility zone count must be 2 or 3 when Zone Awareness is Enabled."
+  }
 }
 
 variable "ebs_volume_size" {
