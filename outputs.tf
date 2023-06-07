d2f66f806b11f59aae65af8460b123c4e2c468ae
Max Lobur <max_lobur@outlook.com>
2023-06-07 19:43:02 +0300

    Support AWS Provider V5

--- outputs.tf
+++ outputs.tf
@@ -1,30 +1,30 @@
 output "security_group_id" {
-  value       = join("", aws_security_group.default.*.id)
+  value       = join("", aws_security_group.default[*].id)
   description = "Security Group ID to control access to the Elasticsearch domain"
 }
 
 output "domain_arn" {
-  value       = join("", aws_elasticsearch_domain.default.*.arn)
+  value       = join("", aws_elasticsearch_domain.default[*].arn)
   description = "ARN of the Elasticsearch domain"
 }
 
 output "domain_id" {
-  value       = join("", aws_elasticsearch_domain.default.*.domain_id)
+  value       = join("", aws_elasticsearch_domain.default[*].domain_id)
   description = "Unique identifier for the Elasticsearch domain"
 }
 
 output "domain_name" {
-  value       = join("", aws_elasticsearch_domain.default.*.domain_name)
+  value       = join("", aws_elasticsearch_domain.default[*].domain_name)
   description = "Name of the Elasticsearch domain"
 }
 
 output "domain_endpoint" {
-  value       = join("", aws_elasticsearch_domain.default.*.endpoint)
+  value       = join("", aws_elasticsearch_domain.default[*].endpoint)
   description = "Domain-specific endpoint used to submit index, search, and data upload requests"
 }
 
 output "kibana_endpoint" {
-  value       = join("", aws_elasticsearch_domain.default.*.kibana_endpoint)
+  value       = join("", aws_elasticsearch_domain.default[*].kibana_endpoint)
   description = "Domain-specific endpoint for Kibana without https scheme"
 }
 
@@ -39,11 +39,11 @@ output "kibana_hostname" {
 }
 
 output "elasticsearch_user_iam_role_name" {
-  value       = join(",", aws_iam_role.elasticsearch_user.*.name)
+  value       = join(",", aws_iam_role.elasticsearch_user[*].name)
   description = "The name of the IAM role to allow access to Elasticsearch cluster"
 }
 
 output "elasticsearch_user_iam_role_arn" {
-  value       = join(",", aws_iam_role.elasticsearch_user.*.arn)
+  value       = join(",", aws_iam_role.elasticsearch_user[*].arn)
   description = "The ARN of the IAM role to allow access to Elasticsearch cluster"
 }
