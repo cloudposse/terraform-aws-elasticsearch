53f99c049adbf200b92ea9096e84ccabf088a390
Max Lobur <max_lobur@outlook.com>
2023-10-16 20:33:54 +0300

    Support AWS Provider V5

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -3,22 +3,26 @@ provider "aws" {
 }
 
 module "vpc" {
-  source                  = "cloudposse/vpc/aws"
-  version                 = "2.1.0"
+  source  = "cloudposse/vpc/aws"
+  version = "2.1.0"
+
   ipv4_primary_cidr_block = var.vpc_cidr_block
-  context                 = module.this.context
+
+  context = module.this.context
 }
 
 module "subnets" {
-  source               = "cloudposse/dynamic-subnets/aws"
-  version              = "2.3.0"
+  source  = "cloudposse/dynamic-subnets/aws"
+  version = "2.4.1"
+
   availability_zones   = var.availability_zones
   vpc_id               = module.vpc.vpc_id
   igw_id               = [module.vpc.igw_id]
   ipv4_cidr_block      = [module.vpc.vpc_cidr_block]
   nat_gateway_enabled  = false
   nat_instance_enabled = false
-  context              = module.this.context
+
+  context = module.this.context
 }
 
 module "elasticsearch" {
