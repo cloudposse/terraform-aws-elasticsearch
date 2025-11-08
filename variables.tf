025fff91e662ea08a9f25d377c631c33b346605c
Niels Voorhoeve <niels.voorhoeve@gmail.com>
2025-11-07 22:14:11 -0500

    Upgrade default TLS Policy domain_endpoint_options_tls_security_policy (#219)
    Co-authored-by: Niels Voorhoeve <niels.websafe@gmail.com>

--- variables.tf
+++ variables.tf
@@ -209,7 +209,7 @@ variable "domain_endpoint_options_enforce_https" {
 
 variable "domain_endpoint_options_tls_security_policy" {
   type        = string
-  default     = "Policy-Min-TLS-1-0-2019-07"
+  default     = "Policy-Min-TLS-1-2-2019-07"
   description = "The name of the TLS security policy that needs to be applied to the HTTPS endpoint"
 }
 
