606eb88ae4b860b628c4e771a402a9487a8ff0f1
Igor Rodionov <496956+goruha@users.noreply.github.com>
2025-11-21 14:47:26 +0100

    Upgrade OpenSearch version to 3.1 (#220)
    * Upgrade OpenSearch version to 3.1
    
    * Upgrade Elasticsearch version to 7.10
    
    * Update Elasticsearch version to 7.10
    
    * Upgrade Elasticsearch version to 7.10
    
    * Update default Elasticsearch version to 7.10

--- examples/basic/main.tf
+++ examples/basic/main.tf
@@ -12,7 +12,7 @@ module "elasticsearch" {
   vpc_id                  = "vpc-XXXXXXXXX"
   subnet_ids              = ["subnet-XXXXXXXXX", "subnet-YYYYYYYY"]
   zone_awareness_enabled  = "true"
-  elasticsearch_version   = "6.5"
+  elasticsearch_version   = "7.10"
   instance_type           = "t2.small.elasticsearch"
   instance_count          = 4
   ebs_volume_size         = 10
